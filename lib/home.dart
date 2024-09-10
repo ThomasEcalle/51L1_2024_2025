d8ffd4eb9c6d0e7e50b29f6a4b9d52c67137f000
ThomasEcalle <thomasecalle@gmail.com>
2024-09-10 13:02:41 +0200

    Add beginning of stateful scaffold

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      child: Center(
-        child: Text(
-          'Coucou',
-          style: Theme.of(context).textTheme.displayMedium,
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        centerTitle: true,
+        leading: const Icon(
+          Icons.airplanemode_active,
         ),
+        actions: const [
+          Icon(Icons.share),
+        ],
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Coucou'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          print('New index: $newIndex');
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
