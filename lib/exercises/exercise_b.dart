bffc09b12d9275f72228c2d7500be5d1c7361e15
ThomasEcalle <thomasecalle@gmail.com>
2024-09-11 16:01:56 +0200

    Add exercise b correction

--- lib/exercises/exercise_b.dart
+++ lib/exercises/exercise_b.dart
@@ -1,28 +1,52 @@
 import 'package:flutter/material.dart';
 
-class ExerciseB extends StatelessWidget {
+class ExerciseB extends StatefulWidget {
   const ExerciseB({super.key});
+
+  @override
+  State<ExerciseB> createState() => _ExerciseBState();
+}
+
+class _ExerciseBState extends State<ExerciseB> {
+  bool _selected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SquareButton(),
+        child: SquareButton(
+          color: _selected ? Colors.amber : Colors.blue,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTap,
+        child: const Icon(Icons.draw),
       ),
     );
+  }
+
+  void _onTap() {
+    setState(() {
+      _selected = !_selected;
+    });
   }
 }
 
 class SquareButton extends StatelessWidget {
-  const SquareButton({super.key});
+  const SquareButton({
+    super.key,
+    this.color = Colors.blue,
+  });
+
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
       height: 100,
       width: 100,
-      color: Colors.orange,
+      color: color,
     );
   }
 }
-
