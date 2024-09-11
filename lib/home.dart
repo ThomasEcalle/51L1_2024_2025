68e650363a2347c01b0763afb7123cc652afe3ae
ThomasEcalle <thomasecalle@gmail.com>
2024-09-11 14:28:03 +0200

    Add scaffold stateful

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'package:al1_2024/home_tab.dart';
+import 'package:al1_2024/settings_tab.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,11 +12,17 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _currentIndex = 1;
 
+  final _tabs = const [
+    HomeTab(),
+    SettingsTab(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
+        backgroundColor: Colors.green,
         centerTitle: true,
         leading: const Icon(
           Icons.airplanemode_active,
@@ -24,17 +32,10 @@ class _HomeState extends State<Home> {
         ],
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Coucou'),
-      ),
+      body: _tabs[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
-        onTap: (int newIndex) {
-          print('New index: $newIndex');
-          setState(() {
-            _currentIndex = newIndex;
-          });
-        },
+        onTap: _onTap,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -46,6 +47,18 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _onTap(1),
+      ),
     );
+  }
+
+  void _onTap(int newIndex) {
+    print('New index: $newIndex');
+    setState(() {
+      _currentIndex = newIndex;
+    });
   }
 }
