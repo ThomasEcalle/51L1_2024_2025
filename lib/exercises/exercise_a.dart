b15cb20ef0e1d421a5014dfa3886397afb3c713c
ThomasEcalle <thomasecalle@gmail.com>
2024-09-11 15:06:28 +0200

    Add exercise a correction

--- lib/exercises/exercise_a.dart
+++ lib/exercises/exercise_a.dart
@@ -5,7 +5,7 @@ class ExerciseA extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: SquareButton(),
       ),
@@ -13,16 +13,46 @@ class ExerciseA extends StatelessWidget {
   }
 }
 
-class SquareButton extends StatelessWidget {
+class SquareButton extends StatefulWidget {
   const SquareButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      color: Colors.blue,
-    );
-  }
+  State<SquareButton> createState() => _SquareButtonState();
 }
 
+class _SquareButtonState extends State<SquareButton> {
+  bool _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        height: _selected ? 200 : 100,
+        width: 100,
+        decoration: BoxDecoration(
+            color: _selected ? Colors.blue : Colors.orange,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(_selected ? 0 : 20),
+              bottomRight: const Radius.circular(20),
+            )),
+        child: Center(
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 500),
+            opacity: _selected ? 1 : 0,
+            child: Text(
+              _selected ? 'Coucou' : 'pas coucou',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTap() {
+    setState(() {
+      _selected = !_selected;
+    });
+  }
+}
