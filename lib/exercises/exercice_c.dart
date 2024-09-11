1cbd65aef7242aa6571b81d78cbd915141fee835
ThomasEcalle <thomasecalle@gmail.com>
2024-09-11 16:30:34 +0200

    Add exercise c correction

--- lib/exercises/exercice_c.dart
+++ lib/exercises/exercice_c.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 
-class ExerciseC extends StatelessWidget {
+class ExerciseC extends StatefulWidget {
   const ExerciseC({super.key});
+
+  @override
+  State<ExerciseC> createState() => _ExerciseCState();
+}
+
+class _ExerciseCState extends State<ExerciseC> {
+  bool _selected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _selected ? Colors.amber : Colors.pink,
       body: Center(
-        child: SquareButton(),
+        child: SquareButton(
+          onTap: _onTap,
+        ),
       ),
     );
+  }
+
+  void _onTap() {
+    setState(() {
+      _selected = !_selected;
+    });
   }
 }
 
 class SquareButton extends StatelessWidget {
-  const SquareButton({super.key});
+  const SquareButton({
+    super.key,
+    this.onTap,
+  });
+
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      color: Colors.blue,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.blue,
+      ),
     );
   }
 }
