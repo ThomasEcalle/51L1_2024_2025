fc43efd6ef8e287d8bd05d2d49ad575662c45a84
ThomasEcalle <thomasecalle@gmail.com>
2024-09-12 10:31:31 +0200

    Add middle of layouts

--- lib/layouts/layouts.dart
+++ lib/layouts/layouts.dart
@@ -1,3 +1,4 @@
+import 'package:al1_2024/layouts/red_circle.dart';
 import 'package:flutter/material.dart';
 
 class Layouts extends StatelessWidget {
@@ -5,41 +6,77 @@ class Layouts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    final screenHeight = mediaQuery.size.height;
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final small = screenWidth < 600;
+
+    final redCircles = List.generate(30, (index) {
+      return const RedCircle();
+    });
+
     return Scaffold(
       body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            final maxHeight = constraints.maxHeight;
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      width: double.infinity,
-                      color: Colors.orange,
-                    ),
-                  ),
-                  Container(
-                    height: 30,
+        child: LayoutBuilder(builder: (context, constraints) {
+          final maxHeight = constraints.maxHeight;
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
                     width: double.infinity,
-                    color: Colors.blue,
-                  ),
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      color: Colors.pink,
+                    color: small ? Colors.green : Colors.orange,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            color: Colors.pink,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            width: double.infinity,
+                            color: Colors.orange,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10,
+                              ),
+                              child: Wrap(
+                                children: redCircles,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            );
-          }
-        ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.pink,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
